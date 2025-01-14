626f45295f9846e86dea7ba4df68c47842e1ceb9
raghavendru <raghavendruppar@gmail.com>
2025-01-14 20:16:36 +0530

    latestup of custer by ran

--- src/config/database.js
+++ src/config/database.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose")
 
 const connectDb = async ()=>{
-    mongoose.connect("mongodb+srv://raghavendr:12345@devtinder.npcym.mongodb.net/DivTinder")
+  mongoose.connect("mongodb+srv://ranjankumarnayak3333:G5sN1oAqbNjJIUvU@cluster0.3pbds.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0/DivTinder")
 
 }
 
